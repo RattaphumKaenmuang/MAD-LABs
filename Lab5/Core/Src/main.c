2b163abc7fc4552efe17ba3855e460cc2ab707d5
Rattaphum Kaenmuang <66010704@KMITL.AC.TH>
2025-08-17 19:10:31 +0700

    ADC, DMA, DMA Interrupt setup

--- Lab5/Core/Src/main.c
+++ Lab5/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "adc.h"
+#include "dma.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -91,6 +92,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_ADC1_Init();
   MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
@@ -105,6 +107,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+
     /* USER CODE BEGIN 3 */
     while (HAL_ADC_PollForConversion(&hadc1, 100) != HAL_OK) {}
     adc_val = HAL_ADC_GetValue(&hadc1);
